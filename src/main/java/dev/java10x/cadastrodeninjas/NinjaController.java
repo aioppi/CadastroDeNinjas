5eb7595768574423965ba286c2b257ec2c50c9c8
Alexandre Giacomoni Ioppi <ioppialexandre@gmail.com>
2026-02-11 21:02:08 +0000

    Cria uma entidade no bando de dados apartir do NinjaModel [Aula 03]

---
+++ src/main/java/dev/java10x/cadastrodeninjas/NinjaController.java
@@ -0,0 +1,17 @@
+package dev.java10x.cadastrodeninjas;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+
+public class NinjaController {
+
+    @GetMapping ("/boasvindas")// Pegar anotação para criar uma rota do tipo GET, ou seja, para receber requisições do tipo GET.
+
+    public String boasvindas () {
+        return "Essa é minha primeira mensagem nessa rota!";
+    }
+
+
+}
